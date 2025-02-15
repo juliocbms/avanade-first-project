e2bab918fc0fd2f9bd6b8edd0ee2b63b0cd3f04c
julio <juliocesar13b@gmail.com>
2025-02-15 14:30:04 -0300

    Implementação na camada de dominio.

---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,46 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_card")
+public class Card {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(unique = true)
+    private String Number;
+
+    @Column(name = "available_limit",scale = 13, precision = 2)
+    private BigDecimal limit;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return Number;
+    }
+
+    public void setNumber(String number) {
+        Number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
