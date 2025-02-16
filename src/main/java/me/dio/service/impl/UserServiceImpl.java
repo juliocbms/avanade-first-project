27050cada9951ebeffcef0a20f0e7968fa4a167c
julio <juliocesar13b@gmail.com>
2025-02-15 22:24:31 -0300

    Finalizando.

--- src/main/java/me/dio/service/impl/UserServiceImpl.java
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -1,13 +1,14 @@
-package me.dio.service;
+package me.dio.service.impl;
 
 import me.dio.domain.model.User;
 import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
